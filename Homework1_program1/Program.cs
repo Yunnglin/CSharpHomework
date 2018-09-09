a421e804f7a0ed9e29f07d1a36db1539939350ef
Yunlin <mao.looper@qq.com>
2018-09-09 19:59:11 +0800

    添加项目文件。

---
+++ Homework1_program1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1_program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "";
+            double num1 = 0;
+            double num2 = 0;
+            Console.Write("Please input the first number: ");
+            s=Console.ReadLine();
+            num1 = Double.Parse(s);
+            Console.Write("Please input the second number: ");
+            s = Console.ReadLine();
+            num2 = Double.Parse(s);
+            Console.WriteLine("The multiple of" + num1 + " and " + num2 + " is " + num1 * num2);
+        }
+    }
+}
---
+++ Homework1_program2/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework1_program2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s1 = textBox1.Text;
+            string s2 = textBox2.Text;
+            double num1 = Double.Parse(s1);
+            double num2 = Double.Parse(s2);
+            double n = num1 * num2;
+            resualt_lab.Text = n.ToString();
+        }
+
+        private void resualt_lab_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

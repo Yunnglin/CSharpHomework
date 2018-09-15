3120ae934eb5ffc26f5c5cdcb7c27f5714292884
Yunlin <mao.looper@qq.com>
2018-09-15 13:43:33 +0800

    更新

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-C#程序设计教程作业
-2017302580290 
-毛云麟
+/*
+@subject: C#程序设计教程作业
+@number: 2017302580290 
+@author: 毛云麟
+*/
+
+Contents
+Homework1:
+	program1:控制台程序，计算两个数的积
+	program2:windows应用程序，计算两个数的积
+	
+Homework2:
+	program1:计算输入数据的素数因子
+	program2:求以整数组的最大值、最小值、平均值、所有元素的和
+	program3:埃氏筛法求2~100内的素数
--- homework2/program1/Program.cs
+++ homework2/program1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace program1
 {
@@ -10,6 +11,59 @@ namespace program1
     {
         static void Main(string[] args)
         {
+            Program program = new Program();
+            do
+            {
+                Console.WriteLine("请输入一个正整数：(输入-1退出程序)");
+                try
+                {
+                    int num = Convert.ToInt32(Console.ReadLine());
+                    if (num == -1)
+                    {
+                        break;
+                    }
+                    if (num <= 0 || num == 1)
+                    {
+                        Console.WriteLine("emmmmm");
+                        continue;
+                    }
+                    Console.Write(num + " = ");
+                    program.Factorization(num);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("请输入正确的数字！！！");
+                    Console.WriteLine();
+                }
+            } while (true);
+        }
+
+        public void Factorization(int num)//计算素因子
+        {
+            ArrayList factors = new ArrayList();
+            for (int i = 2; i < num; i++)
+            {
+                while (num != i)
+                {
+                    if (num % i == 0)
+                    {
+                        factors.Add(i);
+                        num = num / i;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            factors.TrimToSize();
+            foreach (int i in factors)
+            {
+                Console.Write(i + " * ");
+            }
+            Console.Write(num);//跳出时最后的质数
+            Console.WriteLine("");
         }
     }
 }

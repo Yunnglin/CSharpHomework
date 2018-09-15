d0903ae12f0fdfc1f859fa3f56917229fc3123f5
Yunlin <mao.looper@qq.com>
2018-09-15 20:56:49 +0800

    第二次作业

--- README.md
+++ README.md
@@ -1,17 +1,16 @@
-## Subject: C# Programming Tutorial
-
-Author: Mr.Mao  
+### Subject: C# Programming Tutorial
+### Author: Mr.Mao
 -------------
 
-Contents  
+##Contents
 --------
 
-Homework1:  
->program1:	控制台程序，计算两个数的积  
->program2:	Windows应用程序，计算两个数的积  
+Homework1:
+>program1:	控制台程序，计算两个数的积
+>program2:	Windows应用程序，计算两个数的积
 
 
-Homework2:  
->program1:	计算输入数据的素数因子  
->program2:	求以整数组的最大值、最小值、平均值、所有元素的和  
+Homework2:
+>program1:	计算输入数据的素数因子
+>program2:	求以整数组的最大值、最小值、平均值、所有元素的和
 >program3:	埃氏筛法求2~100内的素数  
--- homework2/program3/Program.cs
+++ homework2/program3/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace program3
 {
@@ -10,7 +11,62 @@ namespace program3
     {
         static void Main(string[] args)
         {
+            Program program = new Program();
+            Console.WriteLine("你想要计算多大数值内的素数？");
+            try
+            {
+                int num = Convert.ToInt32(Console.ReadLine());
+                program.SieveOfEratosthenes(num);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("请输入正确的数值！！！");
+            }
 
         }
+
+        static int K = 0;//计数
+
+        public void SieveOfEratosthenes(int num)//埃拉托斯特尼筛法
+        {
+            //生成动态数组
+            ArrayList primes = new ArrayList();
+
+            //储存所需要的数
+            for (int i = 2; i <=num; i++)
+            {
+                primes.Add(i);
+            }
+            //第一次情况
+            int prePrime = 2;
+            int count = primes.Count;
+            int lastNum = (int)primes[count - 1];
+            int j = 0;
+            //开始遍历，最多根号n次
+            while (prePrime * prePrime <= lastNum)
+            {
+                count = primes.Count;
+                lastNum = (int)primes[count - 1];
+                for (int i = 0; i < count; i++)
+                {
+                    K++;
+                    if (((int)primes[i] % prePrime == 0) && ((int)primes[i]!=2))
+                    {//移除掉各种倍数
+                        primes.RemoveAt(i);
+                        count--; 
+                    }
+                }
+                //下一个素数后移一位
+                j++;
+                prePrime = (int)primes[j];
+            }
+
+            foreach(int i in primes)
+            {
+                Console.Write(i + "  ");
+            }
+            Console.WriteLine(K);
+        }
     }
+
 }

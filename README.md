21b85a4c36eae6ac8a3e5f6230f037c28c63a87f
Yunlin <mao.looper@qq.com>
2018-09-15 15:01:12 +0800

    refresh

--- README.md
+++ README.md
@@ -1,20 +1,18 @@
-/*  
 
-@Subject: C#程序设计教程作业
+Subject: C# programming tutorial
 --------------
-@Author: Mr.Mao  
+Author: Mr.Mao  
 --------------
-*/  
  
 **********************
 Contents  
 --------
 
 Homework1:  
->program1:控制台程序，计算两个数的积  
->program2:windows应用程序，计算两个数的积  
+>program1:	控制台程序，计算两个数的积  
+>program2:	Windows应用程序，计算两个数的积  
 	  
 Homework2:  
->program1:计算输入数据的素数因子  
->program2:求以整数组的最大值、最小值、平均值、所有元素的和  
->program3:埃氏筛法求2~100内的素数  
+>program1:	计算输入数据的素数因子  
+>program2:	求以整数组的最大值、最小值、平均值、所有元素的和  
+>program3:	埃氏筛法求2~100内的素数  
---
+++ homework2/program2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            int[] array = new int[] {64,2,3,2};
+            program.MaxNum(array);
+            program.MinNum(array);
+            program.AverNum(array);
+            program.SumNum(array);
+
+        }
+
+        public void MaxNum(int[] array)
+        {
+            int max;
+            max = array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (max < array[i])
+                {
+                    max = array[i];
+                }
+            }
+            Console.WriteLine("最大值为：" + max);
+        }
+
+        public void MinNum(int[] array)
+        {
+            int min;
+            min = array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (min > array[i])
+                {
+                    min = array[i];
+                }
+            }
+            Console.WriteLine("最小值为：" + min);
+        }
+
+        public void AverNum(int[] array)
+        {
+           
+            double sum = 0.0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+            double aver = 0;
+            aver = sum / array.Length;
+            Console.WriteLine("平均值为：" +aver);
+        }
+
+        public void SumNum(int[] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+            Console.WriteLine("总值为：" + sum);
+        }
+    }
+}

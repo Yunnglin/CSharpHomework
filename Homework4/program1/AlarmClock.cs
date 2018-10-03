d4875a2efa6ef05e167d25b2cb8e90487b8f74f6
Yunlin <mao.looper@qq.com>
2018-10-03 18:40:39 +0800

    闹钟程序

---
+++ Homework4/program1/AlarmClock.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Timers;
+
+namespace program1
+{
+    partial class User
+    {
+        //声明事件参数类型
+        //public class AlarmClockEventArgs : EventArgs
+        //{
+        //    public string Times { set; get; }
+        //}
+        ////声明委托类型
+        //public delegate void AlarmClockEventHandler(object sender, AlarmClockEventArgs e);
+
+        //定义事件源（闹钟）
+        public class AlarmClock
+        {
+            //声明事件
+            //public event AlarmClockEventHandler TimeOut;
+
+            public static int Hour;
+            public static int Minute;
+            public static int Second;
+
+           
+
+            public AlarmClock( int hour=0,int minute=0,int second=0)
+            {
+                Hour = hour;
+                Minute = minute;
+                Second = second;
+            }
+
+            public void SetAlarmClock()
+            {
+                Console.WriteLine("Please set the hour: ");
+                int hour = Convert.ToInt32(Console.ReadLine());
+                if (hour < 0 || hour > 23)
+                {
+                    throw new TimeException("Time out of range");
+                }
+
+                Console.WriteLine("Please set the minutes: ");
+                int minute = Convert.ToInt32(Console.ReadLine());
+                if (minute < 0 || minute > 60)
+                {
+                    throw new TimeException("Time out of range");
+                }
+
+                Console.WriteLine("Please set the seconds");
+                int second = Convert.ToInt32(Console.ReadLine());
+                if (second < 0 || second > 60)
+                {
+                    throw new TimeException("Time out of range");
+                }
+
+                Hour = hour;
+                Minute = minute;
+                Second = second;
+            }
+
+
+            public void TimeStart(System.Timers.Timer timer)
+            {
+                timer.Enabled = true;
+                timer.Interval = 1000;//执行间隔时间,单位为毫秒;此时时间间隔为1秒
+                timer.Start();
+                timer.Elapsed += Ring;
+            }
+
+            public static void Ring(object source, ElapsedEventArgs e)
+            {
+
+                if (DateTime.Now.Hour == Hour && DateTime.Now.Minute == Minute && DateTime.Now.Second == Second)  //如果当前时间是10点30分
+                {
+                    Console.WriteLine("It's " + DateTime.Now.ToString()+ " now!!! ");
+                    Console.WriteLine("早く起きなさい！！！");
+                }
+
+            }
+        }
+
+    }
+}
---
+++ Homework4/program1/User.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace program1
+{
+    partial class User
+    {
+        static void Main(string[] args)
+        {//使用事件机制，模拟实现一个闹钟的定时功能，可以设置闹钟，在闹钟时间到了以后，在控制台显示提示信息
+            try
+            {
+                Timer timer = new System.Timers.Timer();
+
+                AlarmClock clock = new AlarmClock();
+                clock.SetAlarmClock();
+                clock.TimeStart(timer);
+            }
+            catch(TimeException e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine("Error: " + e);
+            }
+            finally
+            {
+                Console.WriteLine("Please wait... ...");
+            }
+            Console.ReadKey();
+
+        }
+
+    }
+}
+
+public class TimeException : ApplicationException
+{
+    public TimeException(string message) : base(message)
+    {
+
+    }
+}
